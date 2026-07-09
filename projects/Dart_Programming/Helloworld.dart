a1be31e26bd54b63a46edd5c73bd2cc10c9a510f
Ayush Agarwal <aayushagarwal694@gmail.com>
2026-07-09 18:35:36 +0530

    firstcommit

---
+++ projects/Dart_Programming/Helloworld.dart
@@ -0,0 +1,13 @@
+// Dart SKD (Software Development Kit)
+
+// Commments: Documentation Purpose
+// 1. Single Line Comment (ctrl + /)
+/* 
+Multi-line
+Comments
+*/
+
+void main() {
+  print("Welcome to Dart Programming");
+  // print("I don't want this code to be execute");
+}
