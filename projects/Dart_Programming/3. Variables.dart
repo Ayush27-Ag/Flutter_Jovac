6a4ecf58429469e166c584f8b4bb5b86461f4ef1
Ayush Agarwal <aayushagarwal694@gmail.com>
2026-07-09 19:19:22 +0530

    dart Fundamentals: variable, datatypes, operators

---
+++ projects/Dart_Programming/3. Variables.dart
@@ -0,0 +1,27 @@
+// Dart is Soundly typed Language
+void main() {
+  String name = "Pankaj";
+  // "Pankaj" value is stored in a variable name "name" and it is of type String
+  print(name);
+  // Type checking is enabled at compile time
+  var myName = "Rahul"; // Variable Declaration
+  myName = 2.5; // Variable Assignment
+  print(myName);
+
+  print(myName.runtimeType);
+
+  // Dynamic disable the type checking
+  dynamic myusername = "ABC";
+  myusername = 123;
+  print(myusername);
+
+  // const Identifier compile time
+  const pi = 3.14;
+  print(pi);
+
+  // final define data only runtime
+  final myName1;
+  myName1 = "Pankaj Kapoor";
+  myName1 = "Rahul";
+  print(myName1);
+}
