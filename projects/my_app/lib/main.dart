b2df047f6377453143c451e6b0dc8babfd8cc050
Ayush Agarwal <aayushagarwal694@gmail.com>
2026-07-23 00:53:54 +0530

    Alignment Widget: SafeArea, Row|Column, Stack, Spacer, SizeBox, Align, and Wrap

--- projects/my_app/lib/main.dart
+++ projects/my_app/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import "package:my_app/screens/align_widget.dart";
 import "package:my_app/screens/container_widget.dart";
+import "package:my_app/screens/row_column_widgets.dart";
+import "package:my_app/screens/safearea_widget.dart";
+import "package:my_app/screens/spacer.dart";
+import "package:my_app/screens/stack_widget.dart";
 import "package:my_app/screens/stateful_widget.dart";
 import "package:my_app/screens/stateless_widget.dart";
+import "package:my_app/screens/wrap_widget.dart";
 
 void main() {
   runApp(MyApp());
@@ -12,6 +18,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen1());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: ContainerWidget(),
+      // home: HomeScreen1(),
+      // home: HomeScreen(),
+      // home: SafeAreaWidget(),
+      // home: RowColumnWidgets(),
+      // home: StackWidget(),
+      // home: SpacerWidget(),
+      // home: AlignWidget(),
+      home: WrapWidget(),
+    );
   }
 }
